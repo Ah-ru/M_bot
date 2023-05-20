db0c26df6090e2b909ccdf65c44df1133cf8f6d4
Ah <ah.coder.ru@gmail.com>
2023-05-21 01:16:44 +0300

    v1.3x

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token = "6032634459:AAHeCf7XQKi-8OJC4eEZfZSVCdb4RFVZ5lw"
 database = "Telegram_bot/telegram.db"
+NEW_PASSWD = "0000"
--- main.py
+++ main.py
@@ -1,170 +1,51 @@
 from telebot import TeleBot
 from telebot.types import *
-from contextlib import closing
-from config import *
-import sqlite3
+from config import token, NEW_PASSWD
 import datetime
+from Keyboards import Keyboard
+from sqlite import SQL_Enter
 
 bot = TeleBot(token)
 
-global new_passwd
-new_passwd = '0000'
-
-class Keyboard:
-    @staticmethod
-    def welcome_keyboard():
-        keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/write_message")
-        button2 = KeyboardButton("/admin")
-        return keyboard.add(button1, button2)
-    
-    @staticmethod   
-    def user_keyboard():
-        keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/send")
-        button2 = KeyboardButton("/go_main")
-        return keyboard.add(button1, button2)
-    
-    @staticmethod
-    def main_menu():
-        keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/go_main")
-        return keyboard.add(button1)
-    
-    @staticmethod
-    def admin_keyboard():
-        keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/read_messages")
-        button2 = KeyboardButton("/delete_all_messages")
-        button3 = KeyboardButton("/settings")
-        button4 = KeyboardButton("/go_main")
-        return keyboard.add(button1, button2, button3, button4)
-
-    @staticmethod  
-    def confirm():
-        keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/confirm_deletion_all_messages")
-        button2 = KeyboardButton("/go_main")
-        return keyboard.add(button1, button2)
-    
-    @staticmethod  
-    def confirm_keyboard():
-        keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/Yes")
-        button2 = KeyboardButton("/No")
-        return keyboard.add(button1, button2)
-
-    @staticmethod
-    def new_pass_settings_keyboard():
-        keyboard = InlineKeyboardMarkup(row_width=2)
-        return keyboard.add(InlineKeyboardButton(text="Set new password",callback_data = "passwd"))
-    
-    @staticmethod
-    def after_passwd_keyboard():
-        Keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/back")
-        button2 = KeyboardButton("/go_main")
-        return Keyboard.add(button1, button2)
-
-    @staticmethod
-    def delete():
-        delete  =  ReplyKeyboardRemove()
-        return delete 
-    
-    @staticmethod
-    def all_commands():
-        Keyboard = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-        button1 = KeyboardButton("/all_commands")
-        button2 = KeyboardButton("/go_main")
-        return Keyboard.add(button1, button2)
-
-def clear():
-    global mes, username, user_id, time
-    mes = username = user_id = time = None
-clear()
-
-def exam_admin(admin_id):
-    with closing(sqlite3.connect(database)) as con:
-        with closing(con.cursor()) as tab:
-            global admin_passwd 
-            global passwd 
-            passwd = ' '.join(map(str, tab.execute("SELECT real_password FROM bot_params").fetchall().pop()))
-            try:     
-                admin_passwd = ' '.join(map(str, tab.execute("SELECT password FROM admin WHERE id = (?) ", (admin_id,)).fetchall().pop()))
-            except:
-                admin_passwd = ' '
-
-
 @bot.message_handler(commands=["start", "help", "go_main"])
 def welcome(message: Message):
-    bot.send_message(message.from_user.id, "Привет! Выбери следующие команды:\nДля того что бы оставить сообщение нажмите на кнопку /write_message", reply_markup = Keyboard.welcome_keyboard())
+    bot.send_message(message.from_user.id, "Привет! Выбери следующие команды:", reply_markup = Keyboard.welcome_keyboard())
 
 
-@bot.message_handler(commands=["write_message"])
-def write(message: Message):
-    ans = bot.send_message(message.from_user.id, "Хорошо, напишите сообщение\nПосле отправки его боту\nОБЯЗАТЕЛЬНО нажмите кнопку /send\nДля возвращения на главную, нажмите кнопку /go_main", reply_markup = Keyboard.user_keyboard())
-    bot.register_next_step_handler(ans, insert_message)
+@bot.callback_query_handler(func=lambda call: call.data == "question")
+def write(call: CallbackQuery):
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    ans = bot.send_message(chat_id=call.message.chat.id, text= "Готов принять ваш вопрос)\nНапишите его")
+    bot.register_next_step_handler(ans, post)
 
-    
-def insert_message(message: Message):
-    global mes, username, user_id, time
-    mes = message.text
-    username = message.from_user.username
-    user_id  = message.from_user.id
-    time = message.date
-
-
-@bot.message_handler(commands=["send"])
 def post(message: Message):
-    if mes == None or mes.startswith('/'):
+    if message.text is None or message.text.startswith('/'):
         bot.send_message(message.from_user.id, "Спам и ложные сообщения не записываются в базу.", reply_markup = Keyboard.main_menu())
     else:
-        bot.send_message(message.from_user.id, "Спасибо за сообщение)\nНажмите /go_main для перехода в главное меню", reply_markup = Keyboard.main_menu())
-        with closing(sqlite3.connect(database)) as con:
-            with closing(con.cursor()) as tab:
-                tab.execute("INSERT INTO user_message VALUES (?,?,?,?)", (user_id, username, mes, time))    
-                con.commit()
-        clear()
+        bot.send_message(message.from_user.id, "Спасибо за обращение)\nНажмите /go_main для перехода в главное меню", reply_markup = Keyboard.main_menu())
+        SQL_Enter.post(message.from_user.id, message.from_user.username, message.text, message.date)
 
 
 
 #admin_part
 @bot.message_handler(commands=['admin'])
 def admin_enter(message: Message):
-    admin_id = message.from_user.id
-    with closing(sqlite3.connect(database)) as con:
-        with closing(con.cursor()) as tab:
-            try:
-                admin_passwd = ' '.join(map(str, tab.execute("SELECT password FROM admin WHERE id = (?) ", (admin_id,)).fetchall().pop()))#Pass for 
-                passwd = ' '.join(map(str, tab.execute("SELECT real_password FROM bot_params").fetchall().pop()))
-                if admin_passwd == passwd:
-                    bot.send_message(message.from_user.id, "Pass\nYou are in the admin panel\nYou can click on buttons in menu", reply_markup = Keyboard.admin_keyboard())
-                else:
-                    ans = bot.send_message(message.from_user.id, "Enter admin password", reply_markup = Keyboard.main_menu())
-                    bot.register_next_step_handler(ans, enter_pass)
-            except:
-                ans = bot.send_message(message.from_user.id, "Enter admin password", reply_markup = Keyboard.main_menu())
-                bot.register_next_step_handler(ans, enter_pass)
+    if SQL_Enter.exam_admin(message.from_user.id):
+        bot.send_message(message.from_user.id, "Pass\nYou are in the admin panel\nYou can click on buttons in menu", reply_markup = Keyboard.admin_keyboard())
+    else:
+        ans = bot.send_message(message.from_user.id, "Enter admin password", reply_markup = Keyboard.main_menu())
+        bot.register_next_step_handler(ans, enter_pass)
             
             
-def enter_pass(message: Message):
-    admin_id = message.from_user.id
-    with closing(sqlite3.connect(database)) as con:
-        with closing(con.cursor()) as tab:
-            passwd = ' '.join(map(str, tab.execute("SELECT real_password FROM bot_params").fetchall().pop()))
-            if passwd == message.text:
-                bot.send_message(message.from_user.id, "Pass\nYou are in the admin panel\nYou can click on buttons in menu", reply_markup = Keyboard.admin_keyboard())
-                tab.execute("DELETE FROM admin WHERE id = (?)", (admin_id,))
-                tab.execute("INSERT INTO admin VALUES (?,?)", (admin_id, passwd))
-                con.commit()
-            else:
-                bot.send_message(message.from_user.id, "You enter incorrect password", reply_markup = Keyboard.main_menu())  
-
+def enter_pass(message: Message):# ok
+    if SQL_Enter.enter_pass(message.from_user.id, message.text):
+        bot.send_message(message.from_user.id, "Pass\nYou are in the admin panel\nYou can click on buttons in menu", reply_markup = Keyboard.admin_keyboard())
+    else:
+        bot.send_message(message.from_user.id, "You enter incorrect password", reply_markup = Keyboard.main_menu())  
 
 @bot.message_handler(commands=["all_commands"])
 def read_messages(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd:
+    if SQL_Enter.exam_admin(message.from_user.id):
         bot.delete_message(chat_id=message.chat.id, message_id=message.id - 1)
         bot.delete_message(chat_id=message.chat.id, message_id=message.id - 2)
         bot.send_message(message.from_user.id, "All admin commands:", reply_markup = Keyboard.admin_keyboard())
@@ -174,8 +55,7 @@ def read_messages(message: Message):
 
 @bot.message_handler(commands=["settings", "back"])
 def read_messages(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd:
+    if SQL_Enter.exam_admin(message.from_user.id):
         bot.send_message(message.from_user.id, text = "To go to other admin commands click /all_commands\nOr click /go_main to go to main menu", reply_markup = Keyboard.all_commands()) 
         bot.send_message(message.from_user.id, "Setting commands:", reply_markup = Keyboard.new_pass_settings_keyboard())
     else:
@@ -183,37 +63,28 @@ def read_messages(message: Message):
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "passwd")
-def passwd_button(call: CallbackQuery): 
-    with closing(sqlite3.connect(database)) as con:
-        with closing(con.cursor()) as tab:
-            passwd = ' '.join(map(str, tab.execute("SELECT real_password FROM bot_params").fetchall().pop()))
+def passwd_button(call: CallbackQuery): #ok
     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
-    ans = bot.send_message(chat_id=call.message.chat.id, text= f"Real Password =  {passwd}\nEnter new password", reply_markup = Keyboard.delete())
+    ans = bot.send_message(chat_id=call.message.chat.id, text= f"Real Password =  {SQL_Enter.passwd_button()}\nEnter new password", reply_markup = Keyboard.delete())
     bot.register_next_step_handler(ans, passwd_new)
     
 
 def passwd_new(message : Message):
     bot.send_message(message.from_user.id, text=f"New password = {message.text}\nConfirm password\nClick Yes or No", reply_markup = Keyboard.confirm_keyboard())
-    global new_passwd
-    new_passwd = message.text
+    global NEW_PASSWD
+    NEW_PASSWD= message.text
 
-
-@bot.message_handler(commands=["Yes"])
+@bot.message_handler(commands=["Yes"])#OK
 def pass_yes(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd:
-        bot.send_message(message.from_user.id, text = f"Password was update, password = {new_passwd}\nClick '/back' to go to settings\nClick '/go_main' to go to main menu", reply_markup = Keyboard.after_passwd_keyboard())
-        with closing(sqlite3.connect(database)) as con:
-            with closing(con.cursor()) as tab:
-                tab.execute(f"UPDATE bot_params SET real_password = ('{new_passwd}')")
-                con.commit()
+    if SQL_Enter.exam_admin(message.from_user.id):
+        bot.send_message(message.from_user.id, text = f"Password was update, password = {NEW_PASSWD}\nClick '/back' to go to settings\nClick '/go_main' to go to main menu", reply_markup = Keyboard.after_passwd_keyboard())
+        SQL_Enter.pass_yes(NEW_PASSWD)
     else:
         bot.send_message(message.from_user.id, "sorry, you are not the admin.", reply_markup = Keyboard.main_menu())
     
 @bot.message_handler(commands=["No"])
 def pass_no(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd:
+    if SQL_Enter.exam_admin(message.from_user.id):
         bot.send_message(message.from_user.id, text = "Password wasn't update\nClick '/back' to go to settings\nClick '/go_main' to go to main menu", reply_markup = Keyboard.after_passwd_keyboard())
     else:
         bot.send_message(message.from_user.id, "sorry, you are not the admin.", reply_markup = Keyboard.main_menu())
@@ -221,34 +92,27 @@ def pass_no(message: Message):
 
 @bot.message_handler(commands=["read_messages"])
 def read_messages(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd:
-        with closing(sqlite3.connect(database)) as con:
-            with closing(con.cursor()) as tab:
-                count = 0
-                while True:
-                    try:
-                        us_id = ' '.join(map(str, tab.execute("SELECT id FROM user_message").fetchall().pop(count)))
-                        username = ' '.join(map(str, tab.execute("SELECT username FROM user_message").fetchall().pop(count)))
-                        date = ' '.join(map(str, tab.execute("SELECT time FROM user_message").fetchall().pop(count)))
-                        text = ' '.join(map(str, tab.execute("SELECT message FROM user_message").fetchall().pop(count)))
-                        offset = datetime.timedelta(hours=3)
-                        tz = datetime.timezone(offset, name='МСК')
-                        local_time = datetime.datetime.fromtimestamp(int(date) , tz=tz).strftime('%d-%m-%Y\nTime: %H:%M:%S') 
-                        bot.send_message(message.from_user.id, text = f"№ {count}\nDate: {local_time}\nUsername: {username}\nid: {us_id}\nText:\n{text}")
-                        count +=1
-                    except:
-                        if count == 0:
-                            bot.send_message(message.from_user.id, "No messages.")
-                        break                        
+    if SQL_Enter.exam_admin(message.from_user.id):#NO
+        count = 0
+        while True:
+            try:
+                db = SQL_Enter.read_messages(count)
+                offset = datetime.timedelta(hours=3)
+                tz = datetime.timezone(offset, name='МСК')
+                local_time = datetime.datetime.fromtimestamp(int(db[2]) , tz=tz).strftime('%d-%m-%Y\nTime: %H:%M:%S') 
+                bot.send_message(message.from_user.id, text = f"№ {count}\nDate: {local_time}\nUsername: {db[1]}\nid: {db[0]}\nText:\n{db[3]}")
+                count +=1
+            except:
+                if count == 0:
+                    bot.send_message(message.from_user.id, "No messages.")
+                break                        
     else:
         bot.send_message(message.from_user.id, "sorry, you are not the admin.", reply_markup = Keyboard.main_menu())
 
 
 @bot.message_handler(commands=["delete_all_messages"])
 def delete_all(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd:
+    if SQL_Enter.exam_admin(message.from_user.id):
         bot.send_message(message.from_user.id, "Danger! All messages will delete!", reply_markup = Keyboard.confirm())
     else:
         bot.send_message(message.from_user.id, "sorry, you are not the admin.", reply_markup = Keyboard.main_menu())
@@ -256,14 +120,10 @@ def delete_all(message: Message):
 
 @bot.message_handler(commands=["confirm_deletion_all_messages"])
 def confirm_delete_all(message: Message):
-    exam_admin(message.from_user.id)
-    if passwd == admin_passwd: 
-        with closing(sqlite3.connect(database)) as con:
-            with closing(con.cursor()) as tab:
-                tab.execute("DELETE FROM user_message")
-                con.commit()
+    if SQL_Enter.exam_admin(message.from_user.id):
+        SQL_Enter.confirm_delete_all()
         bot.send_message(message.from_user.id, "all messages was deleted.", reply_markup = Keyboard.admin_keyboard())
     else:
         bot.send_message(message.from_user.id, "sorry, you are not the admin.", reply_markup = Keyboard.main_menu())
 
-bot.polling(non_stop = True) 
+bot.infinity_polling() 
